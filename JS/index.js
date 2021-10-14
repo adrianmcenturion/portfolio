a96b71573b8dd7a3ba78ab7ac9ec9ebad62a8d57
adrianmcenturion <adrianmcenturion@gmail.com>
2021-10-14 13:55:08 -0300

    navbar responsive listo

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const inicio = document.querySelector('.inicio')
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    inicio.classList.toggle("active")
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    inicio.classList.remove("active")
+}
