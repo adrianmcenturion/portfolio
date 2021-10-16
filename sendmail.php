3051aa6ff2a8356612b4582dac8064f9d6adbdf5
adrianmcenturion <adrianmcenturion@gmail.com>
2021-10-15 21:11:45 -0300

    test2

--- sendmail.php
+++ sendmail.php
@@ -19,26 +19,26 @@ $errors = "";
         $headers .= "Reply-To: " . $visitorEmail . "rn";
 
         
+        $success = mail($TARGET_EMAIL, $EMAIL_SUBJECT, $emailBody, $headers);
+        //SUCCESS OR FAIL FOR CORRECT TEXT
+        if ($success){
+                ?>
+              <script language="javascript" type="text/javascript">
+              //REDIRECT TO THE MAIN PAGE or alternatively to 'thank you page'
+         alert('Thank you for the message. I will be in contact with you shortly.');
+               window.location.href = './index.html';
+              </script>
+              <?php
+        } else{
+              //mail failed for some reason
+              ?>
+              <script language="javascript" type="text/javascript">
+                alert('Message failed. Please, contact me by an alternative way.');
+                window.location.href = './index.html';
+              </script>
+              <?php
+        }
 }}
-$success = mail($TARGET_EMAIL, $EMAIL_SUBJECT, $emailBody, $headers);
-//SUCCESS OR FAIL FOR CORRECT TEXT
-if ($success){
-        ?>
-      <script language="javascript" type="text/javascript">
-      //REDIRECT TO THE MAIN PAGE or alternatively to 'thank you page'
- alert('Thank you for the message. I will be in contact with you shortly.');
-       window.location.href = './index.html';
-      </script>
-      <?php
-} else{
-      //mail failed for some reason
-      ?>
-      <script language="javascript" type="text/javascript">
-        alert('Message failed. Please, contact me by an alternative way.');
-        window.location.href = './index.html';
-      </script>
-      <?php
-}
 
 
 ?>
