8043bb71ec8124bf830d45293c8fd69b4e2c38ca
adrianmcenturion <adrianmcenturion@gmail.com>
2021-12-12 00:49:46 -0300

    fix

--- JS/index.js
+++ JS/index.js
@@ -22,15 +22,3 @@ function closeMenu() {
 
 
 
-function redirect() {
-    if (window.location.href = "https://adriancenturion.vercel.app/thankyou.html") {
-
-    setTimeout(window.location.replace("https://adriancenturion.vercel.app"), 2000);
-        
-    }else {
-        return
-    }
-    
-}
-
-redirect()
---
+++ JS/thankyou.js
@@ -0,0 +1,12 @@
+function redirect() {
+    if (window.location.href = "https://adriancenturion.vercel.app/thankyou.html") {
+
+    setTimeout(window.location.replace("https://adriancenturion.vercel.app"), 2000);
+        
+    }else {
+        return
+    }
+    
+}
+
+redirect()
