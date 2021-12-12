efa5e7908019bf48330b70d7a62d6578eb80617b
adrianmcenturion <adrianmcenturion@gmail.com>
2021-12-12 00:53:20 -0300

    test

--- JS/thankyou.js
+++ JS/thankyou.js
@@ -1,7 +1,7 @@
 function redirect() {
     if (window.location.href = "https://adriancenturion.vercel.app/thankyou.html") {
 
-    setTimeout(window.location.replace("https://adriancenturion.vercel.app"), 2000);
+    setTimeout(window.location.replace("https://adriancenturion.vercel.app"), 8000);
         
     }else {
         return
