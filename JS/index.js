e4696050f20ae9df162e56f6847384655520234b
adrianmcenturion <adrianmcenturion@gmail.com>
2021-12-12 00:40:14 -0300

    fix redirect

--- JS/index.js
+++ JS/index.js
@@ -23,7 +23,7 @@ function closeMenu() {
 
 
 function redirect() {
-    if (window.location.href = "https://adriancenturion.vercel.app/thankyou") {
+    if (window.location.href = "https://adriancenturion.vercel.app/thankyou.html") {
 
     setTimeout(window.location.replace("https://adriancenturion.vercel.app"), 2000);
         
